bf23890e991e8f472faccc42d185b4faa8fe6f4d
Nikola045 <80715268+Nikola045@users.noreply.github.com>
2023-03-04 19:42:32 +0100

    Login form

--- InitialProject/InitialProject/Model/User.cs
+++ InitialProject/InitialProject/Model/User.cs
@@ -1,5 +1,6 @@
 ﻿using InitialProject.Serializer;
 using System;
+using System.DirectoryServices.ActiveDirectory;
 
 namespace InitialProject.Model
 {
@@ -8,18 +9,20 @@ namespace InitialProject.Model
         public int Id { get; set; }
         public string Username { get; set; }
         public string Password { get; set; }
+        public string LoginRole { get; set; }
 
         public User() { }
 
-        public User(string username, string password)
+        public User(string username, string password, string loginRole)
         {
             Username = username;
             Password = password;
+            LoginRole = loginRole;
         }
 
         public string[] ToCSV()
         {
-            string[] csvValues = { Id.ToString(), Username, Password };
+            string[] csvValues = { Id.ToString(), Username, Password, LoginRole };
             return csvValues;
         }
 
@@ -28,6 +31,7 @@ namespace InitialProject.Model
             Id = Convert.ToInt32(values[0]);
             Username = values[1];
             Password = values[2];
+            LoginRole = values[3];
         }
     }
 }
---
+++ InitialProject/InitialProject/View/OwnerForm.xaml.cs
@@ -0,0 +1,59 @@
+﻿using InitialProject.Model;
+using InitialProject.Repository;
+using System;
+using System.ComponentModel;
+using System.Runtime.CompilerServices;
+using System.Windows;
+using TravelAgency.Model;
+using TravelAgency.Repository;
+
+namespace InitialProject.Forms
+{
+    /// <summary>
+    /// Interaction logic for CommentForm.xaml
+    /// </summary>
+    public partial class OwnerForm : Window
+    {
+
+        public User LoggedInUser { get; set; }
+
+        public Owner SelectedComment { get; set; }
+
+        private readonly OwnerRepository _repository;
+
+        private string _text;
+        public string Text
+        {
+            get => _text;
+            set
+            {
+                if (value != _text)
+                {
+                    _text = value;
+                    OnPropertyChanged();
+                }
+            }
+        }
+
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        protected virtual void OnPropertyChanged([CallerMemberName] string propertyName = null)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
+        }        
+
+        public OwnerForm(User user)
+        {
+            InitializeComponent();
+            Title = "Create new comment";
+            DataContext = this;
+            LoggedInUser = user;
+            _repository = new OwnerRepository();
+        }
+
+        
+
+        
+
+    }
+}
