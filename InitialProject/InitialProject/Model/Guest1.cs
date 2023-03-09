80dcf04ad57e87d8a7063c3fe625039d55a0491b
ristic02 <ristic02@gmail.com>
2023-03-09 17:28:01 +0100

    Guest1 class update

--- InitialProject/InitialProject/Model/Guest1.cs
+++ InitialProject/InitialProject/Model/Guest1.cs
@@ -8,5 +8,29 @@ namespace TravelAgency.Model
 {
     public class Guest1
     {
+        public int Id { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+
+        public Guest1() { }
+
+        public Guest1(string username, string password)
+        {
+            Username = username;
+            Password = password;
+        }
+
+        public string[] ToCSV()
+        {
+            string[] csvValues = { Id.ToString(), Username, Password };
+            return csvValues;
+        }
+
+        public void FromCSV(string[] values)
+        {
+            Id = Convert.ToInt32(values[0]);
+            Username = values[1];
+            Password = values[2];
+        }
     }
 }
