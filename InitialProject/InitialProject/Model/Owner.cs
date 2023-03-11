12bc592286c05b90513be014620511f44982c0df
Nikola045 <80715268+Nikola045@users.noreply.github.com>
2023-03-11 01:39:43 +0100

    View Hotels done

--- InitialProject/InitialProject/Model/Owner.cs
+++ InitialProject/InitialProject/Model/Owner.cs
@@ -5,6 +5,7 @@ using System.DirectoryServices.ActiveDirectory;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
 
 namespace TravelAgency.Model
 {
@@ -33,6 +34,35 @@ namespace TravelAgency.Model
             Id = Convert.ToInt32(values[0]);
             Username = values[1];
             Password = values[2];
+        }
+
+        public List<Hotel> ReadFromHotelsCsv(string FileName)
+        {
+            List<Hotel> hotels = new List<Hotel>();
+
+            using (StreamReader sr = new StreamReader(FileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    string[] fields = line.Split('|');
+                    Hotel hotel = new Hotel();
+                    hotel.Id = Convert.ToInt32(fields[0]);
+                    hotel.Name = fields[1];
+                    hotel.City = fields[2];
+                    hotel.Country = fields[3];
+                    hotel.TypeOfHotel = fields[4];
+                    hotel.MaxNumberOfGusets = Convert.ToInt32(fields[5]);
+                    hotel.MinNumberOfGusets = Convert.ToInt32(fields[6]);
+                    hotel.NumberOfDaysToCancel = Convert.ToInt32(fields[7]);
+
+                    hotels.Add(hotel);
+
+
+                }
+            }
+            return hotels;
         }
     }
 }
