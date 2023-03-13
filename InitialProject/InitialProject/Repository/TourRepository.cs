d80be0a94c8610c62bd6846a88470849e6548fe3
Nikola045 <80715268+Nikola045@users.noreply.github.com>
2023-03-13 15:21:35 +0100

    Odradjen search za ture

--- InitialProject/InitialProject/Repository/TourRepository.cs
+++ InitialProject/InitialProject/Repository/TourRepository.cs
@@ -46,7 +46,7 @@ namespace TravelAgency.Repository
 
         public List<Tour> ReadFromToursCsv(string FileName)
         {
-            List<Tour> hotels = new List<Tour>();
+            List<Tour> tours = new List<Tour>();
 
             using (StreamReader sr = new StreamReader(FileName))
             {
@@ -74,11 +74,79 @@ namespace TravelAgency.Repository
                     }
 
 
-                    hotels.Add(tour);
+                    tours.Add(tour);
 
                 }
             }
-            return hotels;
+            return tours;
+        }
+
+        public List<Hotel> FindTour(string FileName, string city, string country, string duration, string leng, string num)
+        {
+            List<Tour> tours = new List<Tour>();
+
+            using (StreamReader sr = new StreamReader(FileName))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+
+                    string[] fields = line.Split('|');
+                    Tour tour = new Tour(); 
+                    tour.Id = Convert.ToInt32(fields[0]);
+                    tour.Name = fields[1];
+                    tour.City = fields[2];
+                    tour.Country = fields[3];
+                    tour.Description = fields[4];
+                    tour.Lenguage = fields[5];
+                    tour.MaxNumberOfGuests = Convert.ToInt32(fields[6]);
+                    tour.StartTime = Convert.ToDateTime(fields[7]);
+                    tour.TourDuration = Convert.ToInt32(fields[8]);
+                    for (int i = 0; i < tour.CheckPoints.Count; i++)
+                    {
+                        tour.CheckPoints[i].Id = Convert.ToInt32(fields[i + 9]);
+                        tour.CheckPoints[i].Name = fields[i + 10];
+                    }
+
+                    if (city != "")
+                    {
+                        if (fields[1] == city)
+                        {
+                            tours.Add(tour);
+                        }
+                    }
+                    if (country != "")
+                    {
+                        if (fields[3] == country)
+                        {
+                            tours.Add(tour);
+                        }
+                    }
+                    if (duration != "")
+                    {
+                        if (fields[4] == duration)
+                        {
+                            tours.Add(tour);
+                        }
+                    }
+                    if (leng != "")
+                    {
+                        if (fields[5] == leng)
+                        {
+                            tours.Add(tour);
+                        }
+                    }
+                    if (num != "")
+                    {
+                        if (fields[7] == num)
+                        {
+                            tours.Add(tour);
+                        }
+                    }
+                }
+            }
+            tours = tours.Distinct().ToList();
+            return tours;
         }
 
 
