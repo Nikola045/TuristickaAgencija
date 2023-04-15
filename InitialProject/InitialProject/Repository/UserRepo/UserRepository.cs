96acac213a84f4a890eb1149888e1c2cbdc61c6e
Nikola045 <80715268+Nikola045@users.noreply.github.com>
2023-04-16 00:40:24 +0200

    Update hotel search
    sada kada pretrazis hotele oni kojima je vlasnik super vlasnik oni su na vrhu

--- InitialProject/InitialProject/Repository/UserRepo/UserRepository.cs
+++ InitialProject/InitialProject/Repository/UserRepo/UserRepository.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.Linq;
 using TravelAgency.Domain.Model;
+using System.IO;
+using System;
 
 namespace TravelAgency.Repository.UserRepo
 {
@@ -23,6 +25,26 @@ namespace TravelAgency.Repository.UserRepo
         {
             _users = _serializer.FromCSV(FilePath);
             return _users.FirstOrDefault(u => u.Username == username);
+        }
+
+        public List<User> GetAll() 
+        {
+            List<User> users = new List<User>();
+            using (StreamReader sr = new StreamReader(FilePath))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string line = sr.ReadLine();
+                    string[] fields = line.Split('|');
+                    User user = new User();
+                    user.Id = Convert.ToInt32(fields[0]);
+                    user.Username = fields[1];
+                    user.Password = fields[2];
+                    user.LoginRole = fields[3];
+                    users.Add(user);
+                }
+            }
+            return users;
         }
     }
 }
