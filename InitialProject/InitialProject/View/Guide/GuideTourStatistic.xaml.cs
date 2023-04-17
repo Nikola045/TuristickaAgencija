54b0e6deb111cfda83d3f1dd32051d46a165119f
Cveyaa <bvb123.cvejic@gmail.com>
2023-04-17 14:28:24 +0200

    TourStatistic
    najs

---
+++ InitialProject/InitialProject/View/Guide/GuideTourStatistic.xaml.cs
@@ -0,0 +1,73 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+using TravelAgency.Domain.Model;
+using TravelAgency.Repository;
+
+namespace TravelAgency.View.Guide
+{
+    /// <summary>
+    /// Interaction logic for TourStatistic.xaml
+    /// </summary>
+    public partial class TourStatistic : Window
+    {
+        private readonly TourRepository tourRepository;
+
+        private const string FilePathTour = "../../../Resources/Data/tours.csv";
+
+        private const string FilePathGuestOnTour = "../../../Resources/Data/guestOnTour.csv";
+
+
+        public TourStatistic()
+        {
+            InitializeComponent();
+            tourRepository = new TourRepository();
+        }
+
+        private void PerYearRB_Checked(object sender, RoutedEventArgs e)
+        {
+            CB.IsEnabled = true;
+        }
+
+        private void AllTimeRB_Checked(object sender, RoutedEventArgs e)
+        {
+            CB.IsEnabled = false;
+            Tour tour = new Tour();
+            List<Tour> tours = new List<Tour>();
+            tour = tourRepository.FindMostAttendedTour(FilePathTour);
+            tours.Add(tour);
+            DataPanel.ItemsSource = tours;
+
+        }
+
+        private void FillComboBox(object sender, RoutedEventArgs e)
+        {
+            CB.Items.Add("2023");
+            CB.Items.Add("2022");
+            CB.Items.Add("2021");
+            CB.Items.Add("2020");
+            CB.Items.Add("2019");
+        }
+
+        private void SelectButton_Click(object sender, RoutedEventArgs e)
+        {
+            Tour tour = new Tour();
+            string Year = CB.SelectedItem.ToString();
+            List<Tour> tours = new List<Tour>();
+            tour = tourRepository.FindMostAttendedTourThisYear(FilePathTour,Year);
+            tours.Add(tour);
+            DataPanel.ItemsSource = tours;
+            
+        }
+    }
+}
