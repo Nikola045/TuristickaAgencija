e63be6b8845addb053b3f5a5c6d51e9d4c7f7ee5
ristic02 <ristic02@gmail.com>
2023-06-08 12:56:55 +0200

    Otvaranje foruma gotovo

--- InitialProject/InitialProject/Services/ForumService.cs
+++ InitialProject/InitialProject/Services/ForumService.cs
@@ -13,11 +13,13 @@ namespace TravelAgency.Services
         private readonly ForumRepository forumRepository;
         private readonly ForumCommentRepository forumCommentRepository;
         private readonly OwnerService ownerService;
+        private readonly ReservationService reservationService;
         public ForumService()
         {
             forumRepository = new(InjectorService.CreateInstance<IStorage<Forum>>());
             forumCommentRepository = new(InjectorService.CreateInstance<IStorage<ForumComment>>());
             ownerService = new OwnerService();
+            reservationService = new ReservationService();
         }
 
         public List<Forum> GetNewForums()
@@ -97,6 +99,27 @@ namespace TravelAgency.Services
             }
             forumCommentRepository.Update(selectedComment);
         }
+        public void UpdateUsefull()
+        {
+            List<Forum> forums = forumRepository.GetAll();
+            foreach (Forum forum in forums)
+            {
+                forum.VeryUseful = IsForumVeryUsefulBool(forum);
+                forumRepository.Update(forum);
+            }            
+        }
+        public void CloseForum(Forum selectedForum)
+        {
+            List<Forum> forums = forumRepository.GetAll();
+            for (int i = 0; i < forums.Count; i++)
+            {
+                if (selectedForum.Id == forums[i].Id)
+                {
+                    forums[i].IsActive = false;
+                    forumRepository.Update(forums[i]);
+                }
+            }
+        }
 
         public void CreateCommentOfOwner(User user, ForumComment comment)
         {
@@ -112,7 +135,29 @@ namespace TravelAgency.Services
 
             MessageBox.Show("Comment successfully created");
         }
+        public void CreateCommentOfGuest1(User user, ForumComment comment)
+        {
+            comment.Id = forumCommentRepository.NextId();
+            comment.Username = user.Username;
+            comment.Role = user.LoginRole;
+            string validation = "No";
+            if (HasGuest1VisitedLocation(user.Username, comment.Forum))
+                validation = "Yes";
+            comment.ValidComment = validation;
 
+            forumCommentRepository.Save(comment);
+
+            MessageBox.Show("Comment successfully created");
+        }
+        public bool HasGuest1VisitedLocation(string Guest1Username, Forum forum)
+        {
+            List<string> locations = reservationService.GetGuest1Reservations(Guest1Username);
+            if (locations.Contains(forum.Country + "|" + forum.City))
+            {
+                return true;
+            }
+            else return false;
+        }
         public int CountOwnerComments(Forum forum)
         {
             List<ForumComment> comments = forumCommentRepository.GetAll();
@@ -143,10 +188,25 @@ namespace TravelAgency.Services
         public string IsForumVearyUseful(Forum forum)
         {
             if (CountGuestComments(forum) >= 20 && CountOwnerComments(forum) >= 10)
-                return "Veary useful";
+                return "Very useful";
             else
                 return "";
         }
-
+        public bool IsForumVeryUsefulBool(Forum forum)
+        {
+            if (CountGuestComments(forum) >= 20 && CountOwnerComments(forum) >= 10)
+                return true;
+            else
+                return false;
+        }
+        public void CreateForum(User guest1,Forum forum)
+        {
+            forum.Id = forumRepository.NextId();
+            forum.Guest1 = guest1;
+            forum.NumberOfReplies = 1;
+            forum.Date = DateTime.Now;
+            forum.VeryUseful = IsForumVeryUsefulBool(forum);
+            forumRepository.Save(forum);
+        }
     }
 }
